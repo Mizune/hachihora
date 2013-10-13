84fb906ffed4f2203b06c8e839d8b926e02c5e7e
Mizunekusunoki <willful.slight@gmail.com>
2013-10-13 16:36:50 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hachihora::Application.config.secret_key_base = '213fb65a7f904a921e19028aeb92a93266c017b4549bc1bb9fa4333bc649dabdaffabf6bec76e18993f9a24f3bc67b401caede9c57c385b4340e253b6dffeef7'
